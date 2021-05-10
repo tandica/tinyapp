d7263b77a68af17d7f793a977bd837044aea997c
tandica <williams.tandica@gmail.com>
2021-05-10 15:22:08 -0400

    Fixed route for longURLs

--- express_server.js
+++ express_server.js
@@ -31,7 +31,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-    const templateVars = { shortURL: req.params.shortURL, longURL: req.params.urlDatabase };
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
 });  
 
