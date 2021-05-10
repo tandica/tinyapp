649375a3e59da2e211db274648c6e710aa0c072c
tandica <williams.tandica@gmail.com>
2021-05-10 18:11:18 -0400

    Changes

--- express_server.js
+++ express_server.js
@@ -1,19 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; 
+
 
 //tell Express to use EJS as template engine
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 //create url database with specified websites
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
 };
 
 //start creating different paths to have various pages with different information
 app.get("/", (req, res) => {
-  res.send("Hello!");
+    res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -21,10 +26,17 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-res.send("<html><body>Hello <b>World</b></body></html>\n");
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
+
 //add routes
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
